a78a041ae9d37eeb3610fdc98408153c95a63329
KSKsurigoma <u775032i@ecs.osaka-u.ac.jp>
2018-09-23 17:34:44 +0900

    背景変わらないシーンのタッチ処理を分離

---
+++ Assets/Scripts/TouchWindow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TouchWindow : MonoBehaviour {
+    private int enterCount = 0;
+    string[] names;
+    string[] talks;
+    string NextScene;
+    public UnityEngine.UI.Text NameLabel; // 名前テキスト
+    public UnityEngine.UI.Text TextLabel; // セリフテキスト
+
+    public void Touching()
+    {
+        //タッチがあるかどうか？
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+
+            // タッチ情報を取得する
+            Touch touch = Input.GetTouch(i);
+
+            // ゲーム中ではなく、タッチ直後であればtrueを返す。
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (enterCount == talks.Length)
+                {
+                    SceneManager.LoadScene(NextScene);
+                }
+                else
+                {
+                    NameLabel.text = names[enterCount];
+                    TextLabel.text = talks[enterCount];
+                    enterCount++;
+                }
+            }
+        }
+    }
+
+    public void Setting(string[] names, string[] talks, string NextScene)
+    {
+        this.names = names;
+        this.talks = talks;
+        this.NextScene = NextScene;
+    }
+}
